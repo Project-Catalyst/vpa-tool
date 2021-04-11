f28ffdba844ac9af28ef45083955e33c987abd08
Lucio Baglione <coire1@gmail.com>
2021-04-11 12:11:01 +0200

    Add logo.

--- src/components/CHeader.vue
+++ src/components/CHeader.vue
@@ -1,5 +1,10 @@
 <template>
   <header class="header">
+    <div class="logo">
+      <router-link :to="{ name: 'Home' }">
+        <img src="@/assets/images/logo.png" />
+      </router-link>
+    </div>
     View Title
   </header>
 </template>
@@ -15,5 +20,12 @@ export default {
 header.header {
   text-align: center;
   padding: 20px;
+  .logo {
+    width: 100px;
+    margin: 0px auto;
+    img {
+      width: 100%;
+    }
+  }
 }
 </style>
