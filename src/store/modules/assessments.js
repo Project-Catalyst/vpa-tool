fd75313bc50a5e4803ec7c4b320e1e3bb109bc1d
Lucio Baglione <coire1@gmail.com>
2021-04-20 22:54:01 +0200

    Increase / Decrease reviews on API on assessment update.

--- src/store/modules/assessments.js
+++ src/store/modules/assessments.js
@@ -1,7 +1,15 @@
+import criteria from '@/assets/data/criteria.json'
+const activeCriteria = criteria.map((c) => c.key)
+
+const isReviewed = (el) => {
+  return activeCriteria.some((c) => el[c])
+}
+
 // initial state
 const getDefaultState = () => ({
   all: []
 })
+
 
 const state = getDefaultState()
 
@@ -24,10 +32,33 @@ const mutations = {
   resetState (state) {
     Object.assign(state, getDefaultState())
   },
+  setReviews (state, data) {
+    const assessment = state.all.find(a => parseInt(a.id) === parseInt(data.id))
+    if (assessment) {
+      assessment.reviews = data.reviews
+    }
+  },
   setProp (state, pp) {
     const assessment = state.all.find(a => parseInt(a.id) === parseInt(pp.assId))
     if (assessment) {
+      const initialState = isReviewed(assessment)
       assessment[pp.propModel] = pp.value
+      const finalState = isReviewed(assessment)
+      if (initialState !== finalState) {
+        const assessmentCb = (res) => {
+          if (res.data) {
+            this.commit('assessments/setReviews', {
+              id: assessment.id,
+              reviews: res.data.reviews
+            })
+          }
+        }
+        if (finalState) {
+          this._vm.$http.post(assessment.id).then(assessmentCb)
+        } else {
+          this._vm.$http.delete(assessment.id).then(assessmentCb)
+        }
+      }
     }
   }
 }
