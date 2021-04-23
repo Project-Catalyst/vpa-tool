16e0298bb919b91e29ebfe25158da93e3519275c
Lucio Baglione <coire1@gmail.com>
2021-04-23 13:30:32 +0200

    Adjust home logic for already registered users.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,21 +5,50 @@
       Early development. Please report any bugs.
     </b-message>
 
+    <b-message type="is-info" has-icon v-if="profile.localDb">
+      <p>
+        Hello <strong>{{profile.info.name}}</strong>,<br />
+        your database is already loaded in the app.<br />
+        You can continue your work clicking on the button below.
+      </p>
+
+    </b-message>
+
     <div class="buttons">
       <b-button
         type="is-primary"
         tag="router-link"
-        :to="{ name: 'profile' }"
+        :to="{ name: buttonInfo.link }"
         expanded
       >
-        Start Process</b-button
+        {{ buttonInfo.text }}</b-button
       >
     </div>
   </div>
 </template>
 
 <script>
+import { mapState } from "vuex";
+
 export default {
   name: "Home",
+  computed: {
+    ...mapState({
+      profile: (state) => state.profile
+    }),
+    buttonInfo() {
+      if (this.profile.localDb) {
+        return {
+          text: 'Continue Process',
+          link: 'conditions'
+        }
+      } else {
+        return {
+          text: 'Start Process',
+          link: 'profile'
+        }
+      }
+    },
+  }
 };
 </script>
