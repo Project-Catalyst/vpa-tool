6e11c26db8f373c8270a08203355ba0bea65b9a0
Thomas Stachl <thomas@stachl.me>
2021-04-24 12:29:33 -0700

    add async computed plugin

--- src/main.js
+++ src/main.js
@@ -1,18 +1,15 @@
 import Vue from "vue";
-import Buefy from "buefy";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-import './plugins/axios'
+import "./plugins/axios";
+import "./plugins/buefy";
 import "./plugins/vue-papa-parse";
 import "./plugins/v-tooltip";
-
-import "buefy/dist/buefy.css";
-import "./assets/sass/main.scss";
+import "./plugins/async-computed";
 
 Vue.config.productionTip = false;
-Vue.use(Buefy);
 
 new Vue({
   router,
